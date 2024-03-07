d1b975427078c54c840ada41ea3f715ced834e54
Cfathurrohman <cfathurrohman@gmail.com>
2024-03-07 12:33:33 +0700

    Add Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -75,16 +75,44 @@ class UserController extends Controller
         //     ],
         // );
 
-        $user = UserModel::firstOrNew(
+        // $user = UserModel::firstOrNew(
+        //     [
+        //     'username' => 'manager33',
+        //     'nama' => 'Manager Tiga Tiga',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        //     ],
+        // );
+
+        // $user->save();
+
+        
+        $user = UserModel::create(
             [
-            'username' => 'manager33',
-            'nama' => 'Manager Tiga Tiga',
+            'username' => 'manager44',
+            'nama' => 'Manager44',
             'password' => Hash::make('12345'),
             'level_id' => 2
             ],
         );
+
+        $user->username = 'manager45';
+
+        $user->isDirty(); //true
+        $user->isDirty('username'); //true
+        $user->isDirty('nama'); //false
+        $user->isDirty('nama','username'); //true
+        
+        $user->isClean(); //falsee
+        $user->isClean('username'); //false
+        $user->isClean('nama'); //true
+        $user->isClean('nama','username'); //false
         
         $user->save();
+        
+        $user->isDirty(); //false
+        $user->isClean(); //true
+        dd($user->isDirty());
 
         return view('user', ['data' => $user]);
     }
