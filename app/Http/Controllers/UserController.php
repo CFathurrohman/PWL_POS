fef5337679f9bc9bcd019364b530c7ca2b66f87d
Cfathurrohman <cfathurrohman@gmail.com>
2024-03-07 09:03:36 +0700

    Add Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -24,15 +25,26 @@ class UserController extends Controller
 
         // UserModel::where('username','customer-1')->update($data);
         
-            $data = [
-                'level_id' => 2,
-                'username' => 'manager_tiga',
-                'nama' => 'manager 3',
-                'password' => Hash::make('12345')
-            ];
-            UserModel::create($data);
+        //     $data = [
+        //         'level_id' => 2,
+        //         'username' => 'manager_tiga',
+        //         'nama' => 'manager 3',
+        //         'password' => Hash::make('12345')
+        //     ];
+        //     UserModel::create($data);
 
-        $user = UserModel::all();
+        // $user = UserModel::all();
+
+        // $user = UserModel::find(1);
+
+        // $user = UserModel::where('level_id', 1)->first();
+
+        // $user = UserModel::firstWhere('level_id', 1);
+
+        $user = UserModel::findOr(20, ['username','nama'], function() {
+            abort(404);
+        });
+
         return view('user', ['data' => $user]);
     }
 }
