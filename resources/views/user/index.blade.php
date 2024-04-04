e29e566a0959ecb3f434d341fe4eb21f022d0e0c
Cfathurrohman <cfathurrohman@gmail.com>
2024-04-04 13:29:11 +0700

    Add filtering JS7

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -7,7 +7,7 @@
                 <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
             </div>
         </div>
-        
+
         <div class="card-body">
             @if (session('success'))
                 <div class="alert alert-success">{{ session('success') }}</div>
@@ -15,6 +15,20 @@
             @if ((session('error')))
                 <div class="alert alert-success">{{ session('error') }}</div>
             @endif
+            <div class="row">
+                <div class="col-md-12">
+                    <label class="col-1 control-label col-form-label">Filter:</label>
+                    <div class="col-3">
+                        <select class="form-control" name="level_id" id="level_id" required>
+                            <option value="">- Semua -</option> <!-- Tambahkan opsi "Semua" -->
+                            @foreach ($level as $item)
+                                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                            @endforeach
+                        </select>                        
+                    <small class="form-texxt text-muted">Level Pengguna</small>
+                    </div>
+                </div>
+            </div>
             <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
                 <thead>
                     <tr>
@@ -33,42 +47,56 @@
 @push('css')
 @endpush
 @push('js')
-    <script>
-        $(document).ready(function() {
-            var dataUser = $('#table_user').DataTable({
-                serverSide: true, // serverSide: true, jika ingin menggunakan server side processing
-                ajax: {
-                    "url": "{{ url('user/list') }}",
-                    "dataType": "json",
-                    "type": "POST"
-                },
-                columns: [{
-                    data: "DT_RowIndex", // nomor urut dari laravel datatable addIndexColumn()
+<script>
+    $(document).ready(function() {
+        var dataUser = $('#table_user').DataTable({
+            serverSide: true,
+            ajax: {
+                "url": "{{ url('user/list') }}",
+                "dataType": "json",
+                "type": "POST",
+                "data": function (d) {
+                    d.level_id = $('#level_id').val();
+                }
+            },
+            columns: [
+                {
+                    data: "DT_RowIndex",
                     className: "text-center",
                     orderable: false,
                     searchable: false
-                }, {
+                },
+                {
                     data: "username",
                     className: "",
-                    orderable: true, // orderable: true, jika ingin kolom ini bisa diurutkan
-                    searchable: true // searchable: true, jika ingin kolom ini bisa dicari
-                }, {
+                    orderable: true,
+                    searchable: true
+                },
+                {
                     data: "nama",
                     className: "",
-                    orderable: true, // orderable: true, jika ingin kolom ini bisa diurutkan
-                    searchable: true // searchable: true, jika ingin kolom ini bisa dicari
-                }, {
+                    orderable: true,
+                    searchable: true
+                },
+                {
                     data: "level.level_nama",
                     className: "",
-                    orderable: false, // orderable: true, jika ingin kolom ini bisa diurutkan
-                    searchable: false // searchable: true, jika ingin kolom ini bisa dicari
-                }, {
+                    orderable: false,
+                    searchable: false
+                },
+                {
                     data: "aksi",
                     className: "",
-                    orderable: false, // orderable: true, jika ingin kolom ini bisa diurutkan
-                    searchable: false // searchable: true, jika ingin kolom ini bisa dicari
-                }]
-            });
+                    orderable: false,
+                    searchable: false
+                }
+            ]
         });
-    </script>
+
+        $('#level_id').on('change', function() {
+            dataUser.ajax.reload();
+        });
+    });
+</script>
+
 @endpush
