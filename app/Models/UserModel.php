24da2df02dbc597851dd25cf151da52ba849190c
Cfathurrohman <cfathurrohman@gmail.com>
2024-04-24 07:42:41 +0700

    Add auth JS9

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserModel extends Model
+class UserModel extends Model implements Authenticatable
 {
     use HasFactory;
 
@@ -23,5 +24,37 @@ class UserModel extends Model
     public function level()
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    // Implementing required methods from Authenticatable interface
+
+    public function getAuthIdentifierName()
+    {
+        return 'user_id';
+    }
+
+    public function getAuthIdentifier()
+    {
+        return $this->getAttribute('user_id');
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->getAttribute('password');
+    }
+
+    public function getRememberToken()
+    {
+        return null; 
+    }
+
+    public function setRememberToken($value)
+    {
+        
+    }
+
+    public function getRememberTokenName()
+    {
+        return null; 
     }
 }
