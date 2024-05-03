df726af60891bbd181eddeba035e6e145cec851f
Cfathurrohman <cfathurrohman@gmail.com>
2024-05-03 16:30:56 +0700

    Add RESTful API Login P2 JS10

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+Route::post('/user', LoginController::class)->name('login');
