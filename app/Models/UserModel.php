4eba5cc26b225fef5fb9e1127421b8eb49f05798
Cfathurrohman <cfathurrohman@gmail.com>
2024-05-06 06:06:37 +0700

    Add Eloquent Accessor P1 JS11

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,9 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+
 
 class UserModel extends Authenticatable implements JWTSubject
 {
@@ -37,10 +38,17 @@ class UserModel extends Authenticatable implements JWTSubject
         'username',
         'nama',
         'password',
+        'image'
     ];
 
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    protected function image(): Attribute{
+        return Attribute::make(
+            get: fn ($image) => url('storage/posts/' . $image),
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', RegisterController::class)->name('register');
+Route::post('/register1', RegisterController::class)->name('register1');
 Route::post('/login', LoginController::class)->name('login');
 Route::post('/logout', LogoutController::class)->name('logout');
 
